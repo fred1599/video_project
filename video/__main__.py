b030925588d0cf070ff7af2178b8754ad3d2259f
Fred <frederic.juppont@skillcorner.com>
2023-12-18 18:53:10 +0100

    refactoring __main__.py

--- video/__main__.py
+++ video/__main__.py
@@ -12,9 +12,7 @@ SUPPORTED_VIDEO_FORMATS: List[str] = ["mp4", "avi", "mkv", "mov", "flv", "wmv"]
 
 def is_tool_installed(name: str) -> bool:
     """Checks if a tool is installed"""
-    if shutil.which("ffmpeg") is not None:
-        return True
-    return False
+    return shutil.which(name) is not None
 
 
 def print_colored(text: str, color: str) -> None:
@@ -36,14 +34,57 @@ def check_ffmpeg_and_ffprobe() -> None:
 app = typer.Typer()
 
 
+def convert_video(input: str, output: str, quality: str) -> None:
+    video = Video(input)
+    if output not in SUPPORTED_VIDEO_FORMATS:
+        typer.echo(
+            f"Unsupported video format. Supported formats: {', '.join(SUPPORTED_VIDEO_FORMATS)}"
+        )
+        raise typer.Exit()
+
+    try:
+        for line in video.convert(output, quality):
+            typer.echo(line)
+    except FileNotFoundError as e:
+        typer.echo(str(e))
+        raise typer.Exit()
+    except Exception as e:
+        typer.echo(f"An error occurred during conversion: {str(e)}")
+        raise typer.Exit()
+
+
+def repair_video(input: str) -> None:
+    video = Video(input)
+    repaired_file = video.repair()
+    typer.echo(f"Repaired video saved as: {repaired_file}")
+
+
+def extract_audio_from_video(input: str, output: str) -> None:
+    video = Video(input)
+    if output not in SUPPORTED_AUDIO_FORMATS:
+        typer.echo(
+            f"Unsupported audio format. Supported formats: {', '.join(SUPPORTED_AUDIO_FORMATS)}"
+        )
+        raise typer.Exit()
+
+    extracted_audio = video.extract_audio(output)
+    if extracted_audio:
+        typer.echo(f"Extracted audio saved as: {extracted_audio}")
+    else:
+        typer.echo("Audio extraction failed.")
+
+
 @app.command()  # type: ignore
 def main(
     command: str = typer.Option(
-        ..., "--command", "-c", help="Command to execute (repair or convert)"
+        ...,
+        "--command",
+        "-c",
+        help="Command to execute (convert, repair, extract_audio)",
     ),
     input: str = typer.Option(..., "--input", "-i", help="Path to the video file"),
     output: str = typer.Option(
-        None, "--output", "-o", help="Output format for conversion"
+        None, "--output", "-o", help="Output format for conversion or audio extraction"
     ),
     quality: str = typer.Option(
         "middle",
@@ -52,42 +93,16 @@ def main(
         help="Quality of the conversion (low, middle, high)",
     ),
 ) -> None:
-    video = Video(input)
-
     if command == "convert":
-        if output not in SUPPORTED_VIDEO_FORMATS:
-            typer.echo(
-                f"Unsupported video format. Supported formats: {', '.join(SUPPORTED_VIDEO_FORMATS)}"
-            )
-            raise typer.Exit()
-
-        try:
-            for line in video.convert(output, quality):
-                typer.echo(line)
-        except FileNotFoundError as e:
-            typer.echo(str(e))
-            raise typer.Exit()
-        except Exception as e:
-            typer.echo(f"An error occurred during conversion: {str(e)}")
-            raise typer.Exit()
-
+        convert_video(input, output, quality)
     elif command == "repair":
-        repaired_file = video.repair()
-        typer.echo(f"Repaired video saved as: {repaired_file}")
-
+        repair_video(input)
     elif command == "extract_audio":
-        if output not in SUPPORTED_AUDIO_FORMATS:
-            typer.echo(
-                f"Unsupported audio format. Supported formats: {', '.join(SUPPORTED_AUDIO_FORMATS)}"
-            )
-            raise typer.Exit()
-
-        extracted_audio = video.extract_audio(output)
-
-        if extracted_audio:
-            typer.echo(f"Extracted audio saved as: {extracted_audio}")
-        else:
-            typer.echo("Audio extraction failed.")
+        extract_audio_from_video(input, output)
+    else:
+        typer.echo(
+            "Invalid command. Please use 'convert', 'repair', or 'extract_audio'."
+        )
 
 
 if __name__ == "__main__":
